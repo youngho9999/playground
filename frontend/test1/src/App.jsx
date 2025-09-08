9f41c5929fb838312011af4e7b853473be79969d
youngho <aplblypt@gmail.com>
2025-09-08 18:41:02 +0900

    feat: board 생성

--- frontend/test1/src/App.jsx
+++ frontend/test1/src/App.jsx
@@ -1,41 +1,34 @@
 import { useState } from "react";
 
-function Card({ index, turn, setTurn }) {
-  const [sym, setSym] = useState("");
-
-  function cardClick() {
-    let turnSym = turn ? "X" : "O";
-    setSym(turnSym);
-    setTurn(!turn);
+function Card({ onBlankClick, board, index }) {
+  function handleClick(index) {
+    onBlankClick(index);
+    console.log(index);
   }
 
   return (
-    <div className="kan" onClick={cardClick}>
-      {sym}
+    <div className="kan" onClick={handleClick}>
+      {board[index]}
     </div>
   );
 }
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(""));
   const [turn, setTurn] = useState(true);
+
+  function handleClick(index) {
+    let divCon = turn ? "X" : "O";
+    setBoard(board.map((item, i) => (i === index ? divCon : item)));
+
+    setTurn(!turn);
+  }
 
   return (
     <>
       <div class="board-line">
-        <Card index={0} turn={turn} setTurn={setTurn}></Card>
-        <Card index ={1}turn={turn} setTurn={setTurn}></Card>
-        <Card index ={2}turn={turn} setTurn={setTurn}></Card>
-      </div>
-
-      <div class="board-line">
-        <Card index = {3}turn={turn} setTurn={setTurn}></Card>
-        <Card index = {4}turn={turn} setTurn={setTurn}></Card>
-        <Card index = {5}turn={turn} setTurn={setTurn}></Card>
-      </div>
-      <div class="board-line">
-        <Card index = {6}turn={turn} setTurn={setTurn}></Card>
-        <Card index = {7}turn={turn} setTurn={setTurn}></Card>
-        <Card index = {8}turn={turn} setTurn={setTurn}></Card>
+        <Card onBlankClick={handleClick} board={board} index={0}></Card>
+        <Card onBlankClick={handleClick} board={board} index={1}></Card>
       </div>
     </>
   );
