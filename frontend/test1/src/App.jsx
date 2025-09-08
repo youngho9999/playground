8edd852368080adfa5ee018a4b83547c4c451bac
youngho <aplblypt@gmail.com>
2025-09-08 10:24:42 +0900

    feat: Card 에 prop 으로 turn, setTurn 넘겨줌

--- frontend/test1/src/App.jsx
+++ frontend/test1/src/App.jsx
@@ -1,30 +1,41 @@
-function Card({ name }) {
-  return <div class="kan" onClick={cardClick}>{name}</div>;
-}
+import { useState } from "react";
 
-function cardClick() {
+function Card({ index, turn, setTurn }) {
+  const [sym, setSym] = useState("");
 
+  function cardClick() {
+    let turnSym = turn ? "X" : "O";
+    setSym(turnSym);
+    setTurn(!turn);
+  }
+
+  return (
+    <div className="kan" onClick={cardClick}>
+      {sym}
+    </div>
+  );
 }
 
 function App() {
+  const [turn, setTurn] = useState(true);
+
   return (
     <>
       <div class="board-line">
-        <Card name="X"></Card>
-        <Card name="X"></Card>
-        <Card name="X"></Card>
+        <Card index={0} turn={turn} setTurn={setTurn}></Card>
+        <Card index ={1}turn={turn} setTurn={setTurn}></Card>
+        <Card index ={2}turn={turn} setTurn={setTurn}></Card>
       </div>
 
       <div class="board-line">
-        <Card name="X"></Card>
-        <Card name="X"></Card>
-        <Card name={'Q'}></Card>
+        <Card index = {3}turn={turn} setTurn={setTurn}></Card>
+        <Card index = {4}turn={turn} setTurn={setTurn}></Card>
+        <Card index = {5}turn={turn} setTurn={setTurn}></Card>
       </div>
-
       <div class="board-line">
-        <Card name="X"></Card>
-        <Card name="X"></Card>
-        <Card name="X"></Card>
+        <Card index = {6}turn={turn} setTurn={setTurn}></Card>
+        <Card index = {7}turn={turn} setTurn={setTurn}></Card>
+        <Card index = {8}turn={turn} setTurn={setTurn}></Card>
       </div>
     </>
   );
