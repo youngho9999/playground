6dbc9b9d2209868959be284b407cbfa2de57ba99
youngho <aplblypt@gmail.com>
2025-09-09 14:11:52 +0900

    feat: 나만의 틱택토 완성

--- frontend/test1/src/App.jsx
+++ frontend/test1/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 
 function Card({ onBlankClick, board, index }) {
-  function handleClick(index) {
+  function handleClick() {
     onBlankClick(index);
-    console.log(index);
   }
 
   return (
@@ -19,9 +18,58 @@ function App() {
 
   function handleClick(index) {
     let divCon = turn ? "X" : "O";
-    setBoard(board.map((item, i) => (i === index ? divCon : item)));
 
+    const nextBoard = board.map((item, i) => (i === index ? divCon : item));
+    setBoard(nextBoard);
     setTurn(!turn);
+
+    let winnerFlag = isPlayerWinning(nextBoard, divCon);
+    console.log(winnerFlag);
+    if (winnerFlag) {
+      alert("now player win");
+    }
+
+    if (isGameEnd(nextBoard)) {
+      alert("game ended");
+    }
+  }
+
+  function isPlayerWinning(nextBoard, mark) {
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8], // 가로
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8], // 세로
+      [0, 4, 8],
+      [2, 4, 6], // 대각선
+    ];
+    console.log(mark);
+
+    return winPatterns.some((pattern) => {
+      return pattern.every((index) => nextBoard[index] === mark);
+    });
+
+    for (const pattern of winPatterns) {
+      let checkFlag = true;
+
+      console.log(nextBoard);
+      for (const i of pattern) {
+        if (nextBoard[i] !== mark) {
+          checkFlag = false;
+        }
+      }
+      if (checkFlag) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function isGameEnd(nextBoard) {
+    return nextBoard.every((cell) => cell === "O" || cell === "X");
   }
 
   return (
@@ -29,6 +77,17 @@ function App() {
       <div class="board-line">
         <Card onBlankClick={handleClick} board={board} index={0}></Card>
         <Card onBlankClick={handleClick} board={board} index={1}></Card>
+        <Card onBlankClick={handleClick} board={board} index={2}></Card>
+      </div>
+      <div class="board-line">
+        <Card onBlankClick={handleClick} board={board} index={3}></Card>
+        <Card onBlankClick={handleClick} board={board} index={4}></Card>
+        <Card onBlankClick={handleClick} board={board} index={5}></Card>
+      </div>
+      <div class="board-line">
+        <Card onBlankClick={handleClick} board={board} index={6}></Card>
+        <Card onBlankClick={handleClick} board={board} index={7}></Card>
+        <Card onBlankClick={handleClick} board={board} index={8}></Card>
       </div>
     </>
   );
